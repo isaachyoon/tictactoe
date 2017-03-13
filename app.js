6f52d38e5902390e48848a6e97b40962b6df5d05
Isaac Yoon <ysaacioon@yahoo.com>
2017-03-13 12:58:46 -0700

    player toggles

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 
 
-
+let flag = 'player1'
 var classArr = document.getElementsByClassName("tiles");
 
 var myFunction = function() {
+	if(flag === 'player1') {
+		this.style.backgroundColor="#FDFF47";
+		document.getElementById('player').innerHTML = 'player 2s turn';
+		flag = 'player2'
+	} else {
+		this.style.backgroundColor='blue';
+		document.getElementById('player').innerHTML = 'player 1s turn';
+		flag = 'player1'
+	}
 	console.log(this);
-	this.style.backgroundColor="#FDFF47";
-  var attribute = this.getAttribute(this);
-  console.log(attribute);
+  // var attribute = this.getAttribute(this);
+
 };
 
 for (var i = 0; i < classArr.length; i++) {
     classArr[i].addEventListener('click', myFunction, false);
 }
 
+var playerToggle = function(){
 
-document.getElementById('player').innerHTML = 'player 1 start';
+}
