4dcae692270d0a667bc9e5e3e5eaec22a2cd4fb7
Isaac Yoon <ysaacioon@yahoo.com>
2017-03-13 11:47:46 -0700

    add highlight feature

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 
 
 
-	var classArr = document.getElementsByClassName('tiles');
-	console.log('classArr', classArr.length);
-	classArr.forEach(el => {
-		el.addEventListener('click', e=> {
-			console.log(e);
-		})
-	})
-	// console.log(className);
+var classArr = document.getElementsByClassName("tiles");
+
+var myFunction = function() {
+	console.log(this);
+	this.style.backgroundColor="#FDFF47";
+  var attribute = this.getAttribute(this);
+  console.log(attribute);
+};
+
+for (var i = 0; i < classArr.length; i++) {
+    classArr[i].addEventListener('click', myFunction, false);
+}
 
 
-	// .addEventListener('click', ()=> {
-	// 		console.log('clicked');
-	// 	})
-
-
-
-
+document.getElementById('player').innerHTML = 'player 1 start';
